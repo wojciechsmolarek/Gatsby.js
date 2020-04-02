29eeb8034528814965856cad0c90fe4cf5b3eb70
Wojciech Smolarek <kontakt@wojciechsmolarek.pl>
2020-04-02 15:23:47 +0200

    edit footer.js file

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = (props) => (
                 <li><a href="#" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
                 <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
                 <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
-                <li><a href="#" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
+                <li><a href="https://github.com/wojciechsmolarek/" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
                 <li><a href="#" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
             </ul>
             <ul className="copyright">
